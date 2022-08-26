6d6bb18891f2acc12bac140cb81a56eda0b1bc31
solomonbarayev <sbarayev2@gmail.com>
2022-08-26 12:17:17 +0300

    add state for isComplete and conditional styling with styled components

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { FiEdit, FiTrash2 } from "react-icons/fi";
+import React, { useState } from 'react';
+import { FiEdit, FiTrash2 } from 'react-icons/fi';
+import styled from 'styled-components';
 
 function Task(props) {
+  const [isComplete, setIsComplete] = useState(false);
+
   return (
-    <li className="task">
+    <TaskItem
+      className="task"
+      onDoubleClick={() => setIsComplete(!isComplete)}
+      isComplete={isComplete}
+    >
       <div>
-        <input type="checkbox" />
-        <p className="task-text">{props.task.text}</p>
+        <Checkbox
+          type="checkbox"
+          checked={isComplete}
+          onChange={() => setIsComplete(!isComplete)}
+        />
+        <TaskText className="task-text" isComplete={isComplete}>
+          {props.task.text}
+        </TaskText>
       </div>
       <div className="button-container">
         <button onClick={() => props.onDeleteTask(props.task.id)}>
@@ -16,8 +29,27 @@ function Task(props) {
           <FiEdit className="task-icon" />
         </button>
       </div>
-    </li>
+    </TaskItem>
   );
 }
 
 export default Task;
+
+// Styled Components
+const TaskItem = styled.li`
+  cursor: pointer;
+  background-color: ${(props) => props.isComplete && '#303030'};
+  border-radius: 5px;
+  border: ${(props) => props.isComplete && 'none'};
+  border-left: ${(props) => props.isComplete && '5px solid orange'};
+  opacity: ${(props) => props.isComplete && '0.7'};
+`;
+
+const TaskText = styled.p`
+  text-decoration: ${({ isComplete }) =>
+    isComplete ? 'line-through' : 'none'};
+`;
+
+const Checkbox = styled.input`
+  cursor: pointer;
+`;
